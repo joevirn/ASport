2c55fbd77f8712794dbeccb31bf6df03534dbeb9
joevirn <69099488+joevirn@users.noreply.github.com>
2022-10-18 00:45:54 +0800

    Edits to database connection file.

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "MyFinance");
+$con=mysqli_connect("localhost", "root", "", "ASport");
 if(mysqli_connect_errno()){
   echo "Connection Fail".mysqli_connect_error();
 }
