d86c1df8326498edce6ed87d160497aab0216601
joevirn <69099488+joevirn@users.noreply.github.com>
2022-10-18 19:11:46 +0800

    Add include file for business header.

---
+++ includes/business-head-styles.php
@@ -0,0 +1,13 @@
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- <link href="css/font-awesome.min.css" rel="stylesheet"> -->
+<link href="css/datepicker3.css" rel="stylesheet">
+<link href="css/styles.css" rel="stylesheet">
+<link rel="icon" type="image/png" href="images/ASport.png" sizes="113x113" >
+
+<!--Custom Font-->
+<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<script src="js/respond.min.js"></script>
+<![endif]-->
