c3a5078c3c694e6fbaae267efb70d2a97570f6c4
joevirn <69099488+joevirn@users.noreply.github.com>
2022-10-19 17:45:59 +0800

    Updates to user new booking page.

--- user-bookings-new-complex.php
+++ user-bookings-new-complex.php
@@ -22,18 +22,9 @@ if (strlen($_SESSION['detsuid'] == 0)) {
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!--title of the webpage-->
 		<title>ASport | New Booking - Relau Sports Complex</title>
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/font-awesome.min.css" rel="stylesheet">
-		<link href="css/datepicker3.css" rel="stylesheet">
-		<link href="css/styles.css" rel="stylesheet">
-		<link rel="icon" type="image/png" href="images/ASport.png" sizes="113x113" >
-
-		<!--Custom Font-->
-		<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-		<!--[if lt IE 9]>
-		<script src="js/html5shiv.js"></script>
-		<script src="js/respond.min.js"></script>
-		<![endif]-->
+		<?php
+	  include('includes/user-head-styles.php');
+	  ?>
 
 		<style>
 			table {
@@ -103,22 +94,23 @@ if (strlen($_SESSION['detsuid'] == 0)) {
 
 			<div class="row">
 
-				<div class="col-md-8">
+				<form role="form" method="post" action="user-bookings-new-confirmation.php">
+				<div class="col-md-7">
 					<div class="panel panel-default">
 						<div class="panel-body">
 	            <h3 align="center"><b>Make A New Booking</b></h3>
-	            <form role="form" method="post" action="user-bookings-new-relausportscomplex-confirmation.php">
 	              <div class="col-md-1"></div>
 	  						<div class="col-md-10">
+									<h5><b>Step 1: Select Category & Date</b></h5>
 									<div class="form-group">
 										<label><span class="fa fa-list"></span>&nbsp Category</label>
 										<select class="form-control" name="bookingCategory">
 											<option value="" selected="true" disabled="disabled">SELECT CATEGORY</option>
 											<?php
-												$sql = $con->query("SELECT name FROM income_categories WHERE userID='".$_SESSION['detsuid']."' ORDER BY position");
-												while($data = $sql->fetch_array()) {
-													echo "<option>".$data['name']."</option>";
-												}
+												// $sql = $con->query("SELECT name FROM income_categories WHERE userID='".$_SESSION['detsuid']."' ORDER BY position");
+												// while($data = $sql->fetch_array()) {
+												// 	echo "<option>".$data['name']."</option>";
+												// }
 											?>
 										</select>
 									</div>
@@ -126,12 +118,69 @@ if (strlen($_SESSION['detsuid'] == 0)) {
 	                  <label><span class="fa fa-calendar"></span>&nbsp Date</label>
 	                  <input class="form-control" type="date" name="bookingDate" required="true">
 	                </div>
+
+									<h5><b>Step 2: Check Available Slots</b></h5>
+									<label><span class="fa fa-table"></span>&nbsp <a href="#liveAvailability">View Live Availability</a></label>
+
+									<h5><b>Step 3: Select Available Slots</b></h5>
 									<div class="form-group">
-										<label><span class="fa fa-calendar"></span>&nbsp Select Available Slot to Book</label>
+										<label><span class="fa fa-info-circle"></span>&nbsp Facility No</label>
+										<select class="form-control" name="bookingFacilityNo">
+											<option value="" selected="true" disabled="disabled">SELECT AVAILABLE COURT</option>
+											<?php
+												// $sql = $con->query("SELECT name FROM income_categories WHERE userID='".$_SESSION['detsuid']."' ORDER BY position");
+												// while($data = $sql->fetch_array()) {
+												// 	echo "<option>".$data['name']."</option>";
+												// }
+											?>
+										</select>
+									</div>
+									<div class="form-group">
+										<label><span class="fa fa-clock-o"></span>&nbsp Start Time</label>
+										<select class="form-control" name="bookingStartTime">
+											<option value="" selected="true" disabled="disabled">SELECT AVAILABLE START TIME</option>
+											<?php
+												// $sql = $con->query("SELECT name FROM income_categories WHERE userID='".$_SESSION['detsuid']."' ORDER BY position");
+												// while($data = $sql->fetch_array()) {
+												// 	echo "<option>".$data['name']."</option>";
+												// }
+											?>
+										</select>
+									</div>
+									<div class="form-group">
+										<label><span class="fa fa-list"></span>&nbsp Duration</label>
+										<select class="form-control" name="bookingDuration">
+											<option value="" selected="true" disabled="disabled">SELECT AVAILABLE DURATION</option>
+											<?php
+												// $sql = $con->query("SELECT name FROM income_categories WHERE userID='".$_SESSION['detsuid']."' ORDER BY position");
+												// while($data = $sql->fetch_array()) {
+												// 	echo "<option>".$data['name']."</option>";
+												// }
+											?>
+										</select>
+									</div>
+									<br>
+									<div align="center" class="form-group has-success">
+										<button type="submit" class="btn btn-primary" name="submit" style="width: 50%;">NEXT</button>
+									</div>
+	              </div>
+								<div class="col-md-1"></div>
+						</div><!-- /.panel-body-->
+					</div><!-- /.panel-->
+
+					<a id="liveAvailability"></a>
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<h3 align="center"><b>Live Availability</b></h3>
+							<br>
+								<div class="col-md-1"></div>
+								<div class="col-md-10">
+									<div class="form-group">
+										<label><span class="fa fa-table"></span>&nbsp Available Check</label>
 										<table id="legend">
 											<tr>
-									       <th id="Legend" style="width:10%;">Legend:</th>
-									       <td id="Available" style="width:10%; text-align:right;">Available</td>
+												 <th id="Legend" style="width:10%;">Legend:</th>
+												 <td id="Available" style="width:10%; text-align:right;">Available</td>
 												 <td id="AvailableColor" style="width:10%;" class="available">RM</td>
 												 <td id="Booked" style="width:10%; text-align:right;">Booked</td>
 												 <td id="BookedColor" style="width:10%;" class="booked">B</td>
@@ -139,37 +188,37 @@ if (strlen($_SESSION['detsuid'] == 0)) {
 												 <td id="LockedColor" style="width:10%;" class="locked">L</td>
 												 <td id="Selection" style="width:10%; text-align:right;">Selection</td>
 												 <td id="SelectionColour" style="width:10%;" class="selection"></td>
-									    </tr>
+											</tr>
 										</table>
 										<br>
 										<table id="bookingTable" name="bookingTable" verifybooking="false">
 										 <thead>
-										    <tr>
-										       <th id="Time" style="width:10%;">Time</th>
-										       <th id="C1" style="width:10%;">C1</th>
-										       <th id="C2" style="width:10%;">C2</th>
-										       <th id="C3" style="width:10%;">C3</th>
-										       <th id="C4" style="width:10%;">C4</th>
-										       <th id="C5" style="width:10%;">C5</th>
-										       <th id="C6" style="width:10%;">C6</th>
-										       <th id="C7" style="width:10%;">C7</th>
-										       <th id="C8" style="width:10%;">C8</th>
-										       <th id="C9" style="width:10%;">C9</th>
-										       <th id="C10" style="width:10%;">C10</th>
-										    </tr>
-										    <tr>
-										       <th id="T1" price="15" startTime="0900" endTime="1000" timeSlotname="0900">09:00</td>
-										       <td id="20221001-T1-C1" price="28" status="0" courtname="Court 1" group="1" style="width:10%;" class="available">RM28</td>
-										       <td id="20221001-T1-C2" price="28" status="0" courtname="Court 2" group="1" style="width:10%;" class="available">RM28</td>
-										       <td id="20221001-T1-C3" price="28" status="1" courtname="Court 3" group="1" style="width:10%;" class="booked">B</td>
-										       <td id="20221001-T1-C4" price="28" status="0" courtname="Court 4" group="1" style="width:10%;" class="available">RM28</td>
-										       <td id="20221001-T1-C5" price="28" status="1" courtname="Court 5" group="1" style="width:10%;" class="booked">B</td>
-										       <td id="20221001-T1-C6" price="28" status="0" courtname="Court 6" group="1" style="width:10%;" class="locked">L</td>
-										       <td id="20221001-T1-C7" price="28" status="0" courtname="Court 7" group="1" style="width:10%;" class="available">RM28</td>
-										       <td id="20221001-T1-C8" price="28" status="0" courtname="Court 8" group="1" style="width:10%;" class="available">RM28</td>
-										       <td id="20221001-T1-C9" price="28" status="0" courtname="Court 9" group="1" style="width:10%;" class="available">RM28</td>
-										       <td id="20221001-T1-C10" price="28" status="0" courtname="Court 10" group="1" style="width:10%;" class="available">RM28</td>
-										    </tr>
+												<tr>
+													 <th id="Time" style="width:10%;">Time</th>
+													 <th id="C1" style="width:10%;">C1</th>
+													 <th id="C2" style="width:10%;">C2</th>
+													 <th id="C3" style="width:10%;">C3</th>
+													 <th id="C4" style="width:10%;">C4</th>
+													 <th id="C5" style="width:10%;">C5</th>
+													 <th id="C6" style="width:10%;">C6</th>
+													 <th id="C7" style="width:10%;">C7</th>
+													 <th id="C8" style="width:10%;">C8</th>
+													 <th id="C9" style="width:10%;">C9</th>
+													 <th id="C10" style="width:10%;">C10</th>
+												</tr>
+												<tr>
+													 <th id="T1" price="15" startTime="0900" endTime="1000" timeSlotname="0900">09:00</td>
+													 <td id="20221001-T1-C1" price="28" status="0" courtname="Court 1" group="1" style="width:10%;" class="available">RM28</td>
+													 <td id="20221001-T1-C2" price="28" status="0" courtname="Court 2" group="1" style="width:10%;" class="available">RM28</td>
+													 <td id="20221001-T1-C3" price="28" status="1" courtname="Court 3" group="1" style="width:10%;" class="booked">B</td>
+													 <td id="20221001-T1-C4" price="28" status="0" courtname="Court 4" group="1" style="width:10%;" class="available">RM28</td>
+													 <td id="20221001-T1-C5" price="28" status="1" courtname="Court 5" group="1" style="width:10%;" class="booked">B</td>
+													 <td id="20221001-T1-C6" price="28" status="0" courtname="Court 6" group="1" style="width:10%;" class="locked">L</td>
+													 <td id="20221001-T1-C7" price="28" status="0" courtname="Court 7" group="1" style="width:10%;" class="available">RM28</td>
+													 <td id="20221001-T1-C8" price="28" status="0" courtname="Court 8" group="1" style="width:10%;" class="available">RM28</td>
+													 <td id="20221001-T1-C9" price="28" status="0" courtname="Court 9" group="1" style="width:10%;" class="available">RM28</td>
+													 <td id="20221001-T1-C10" price="28" status="0" courtname="Court 10" group="1" style="width:10%;" class="available">RM28</td>
+												</tr>
 												<tr>
 													 <th id="T2" price="15" startTime="1000" endTime="1100" timeSlotname="1000">10:00</td>
 													 <td id="20221001-T2-C1" price="28" status="0" courtname="Court 1" group="1" style="width:10%;" class="available">RM28</td>
@@ -232,18 +281,29 @@ if (strlen($_SESSION['detsuid'] == 0)) {
 											echo "</p>";
 										}
 									?>
-	                <br>
-	              </div>
+									<br>
+								</div>
 								<div class="col-md-1"></div>
-
-	  					</form>
-
 						</div><!-- /.panel-body-->
 					</div><!-- /.panel-->
+
 				</div><!-- /.col-->
+				</form>
 
+				<div class="col-md-5">
+					<div class="panel panel-default">
+						<div class="panel-body easypiechart-panel">
+							<h3><b>Facility Layout</b></h3>
+								<table>
+									<tr>
+										<td style="border:none;" width="100%"><img src="images/badminton-court-layout.png" width="200" height="200"></td>
+									</tr>
+								</table>
+						</div>
+					</div>
+				</div>
 
-				<div class="col-md-4">
+				<div class="col-md-5">
 					<div class="panel panel-default">
 						<div class="panel-body easypiechart-panel">
 							<h3><b>Venue Information</b></h3>
