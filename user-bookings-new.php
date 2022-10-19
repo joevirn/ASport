96acc7c36849a64c68d3469b78cb82062af87356
joevirn <69099488+joevirn@users.noreply.github.com>
2022-10-19 17:47:01 +0800

    Updates to new and past booking for user portal.

--- user-bookings-new.php
+++ user-bookings-new.php
@@ -22,18 +22,9 @@ if (strlen($_SESSION['detsuid'] == 0)) {
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!--title of the webpage-->
 		<title>ASport | New Booking</title>
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/font-awesome.min.css" rel="stylesheet">
-		<link href="css/datepicker3.css" rel="stylesheet">
-		<link href="css/styles.css" rel="stylesheet">
-		<link rel="icon" type="image/png" href="images/ASport.png" sizes="113x113" >
-
-		<!--Custom Font-->
-		<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-		<!--[if lt IE 9]>
-		<script src="js/html5shiv.js"></script>
-		<script src="js/respond.min.js"></script>
-		<![endif]-->
+		<?php
+	  include('includes/user-head-styles.php');
+	  ?>
 	</head>
 	<!--end of head-->
 
