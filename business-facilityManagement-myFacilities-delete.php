a47d027daf5693c431edcd19e409c742368df8ea
joevirn <69099488+joevirn@users.noreply.github.com>
2022-10-29 00:36:46 +0800

    Modifications to delete script to move facility layout image file to deleted folder if present.

--- business-facilityManagement-myFacilities-delete.php
+++ business-facilityManagement-myFacilities-delete.php
@@ -13,13 +13,57 @@ else {
 	$businessID = $_SESSION['ASportBusinessSessionCounter'];
 	$businessFacilityID = $_GET['businessFacilityID'];
 
-	$sql = "DELETE FROM businessFacility WHERE businessFacilityID='$businessFacilityID' AND businessID='$businessID'";
-	$result = mysqli_query($con,$sql);
+	//GET CURRENT FACILITY LAYOUT FILE PATH
+	$sqlGetFacilityLayout = "SELECT * FROM businessFacility WHERE businessFacilityID='$businessFacilityID' AND businessID='$businessID'";
+	$facilityLayoutResult = mysqli_query($con,$sqlGetFacilityLayout);
+	while ($data = $facilityLayoutResult->fetch_assoc()){
+		$current_layoutFileName = $data['layoutFileName'];
+	}
 
+	//IF FILE PRESENT, DELETE FACILITY LAYOUT IMAGE FILE BY DELETING PATH FROM DATABASE & MOVING TO THE FILE TO DELETED FOLDER
+	if ($current_layoutFileName) {
+		/* Store the path of source file */
+		$sourceFilePath = $current_layoutFileName;
+		/* Store the path of destination file */
+		$destinationFilePath = './uploads/deleted/'.basename($sourceFilePath);
+		//FILE DELETION QUERY
+		$sqlDeleteFacilityLayout = mysqli_query($con,
+			"UPDATE
+				businessFacility
+			SET
+				layoutFileName = NULL,
+				layoutFileUploadTimeStamp = NULL,
+				timeStampEdited = now()
+			WHERE
+				businessFacilityID = '$businessFacilityID'
+				AND
+				businessID = '$businessID'
+			");
+		// JAVASCRIPT ALERT STATUS
+		if($sqlDeleteFacilityLayout){
+			$statusMsg = 'Facility layout file path has been deleted from database.';
+			echo "<script>alert('$statusMsg');</script>";
+			//move image file from existing folder (./uploads/) to deleted folder (./uploads/deleted)
+			if(rename($sourceFilePath, $destinationFilePath)) {
+				$statusMsg = 'File has been moved to the deleted folder at '.$destinationFilePath.'';
+				echo "<script>alert('$statusMsg');</script>";
+			}
+			else {
+				$statusMsg = 'There is a problem moving this file into the deleted folder. Please try again.';
+				echo "<script>alert('$statusMsg');</script>";
+			}
+		}
+		else {
+			$statusMsg = 'There is a problem deleting this facility layout. Please try again.';
+			echo "<script>alert('$statusMsg');</script>";
+		}
+	}
+
+	//DELETE FACILITY BASED ON FACILITY ID
+	$sqlDeleteFacility = "DELETE FROM businessFacility WHERE businessFacilityID='$businessFacilityID' AND businessID='$businessID'";
+	$deleteResult = mysqli_query($con,$sqlDeleteFacility);
 	// JAVASCRIPT ALERT STATUS
-	//
-	// Facility Add Status
-	if($result){
+	if($deleteResult){
 		echo "<script>alert('Facility has been deleted!');</script>";
 		echo "<script>window.location.href='business-facilityManagement-myFacilities1.php'</script>";
 	}
