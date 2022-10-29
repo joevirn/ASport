9c8edebea21c797b2686010e0c6ae040ce2727db
joevirn <69099488+joevirn@users.noreply.github.com>
2022-10-30 03:31:56 +0800

    Updates to business change password page.

--- business-settings-changePassword.php
+++ business-settings-changePassword.php
@@ -1,4 +1,4 @@
-<!-- <?php
+<?php
 //start the session
 session_start();
 error_reporting(0);
@@ -15,24 +15,32 @@ else {
 	if(isset($_POST['submit'])) {
 		$currentPassword = md5($_POST['currentPassword']);
 		$newPassword = md5($_POST['newPassword']);
+		$confirmPassword = md5($_POST['confirmPassword']);
 		$query = mysqli_query($con,"SELECT businessID FROM business WHERE businessID='$businessID' AND businessPassword='$currentPassword'");
 		$row = mysqli_fetch_array($query);
 		if($row>0){
-			$ret = mysqli_query($con,"UPDATE business SET businessPassword='$newPassword' WHERE businessID='$businessID'");
-			$msg = "Your password is successfully changed!";
+			if ($newPassword!=$currentPassword) {
+				$ret = mysqli_query($con,"UPDATE business SET businessPassword='$newPassword', businessLastPasswordChangeDate=now() WHERE businessID='$businessID'");
+				$msg = "Your password is successfully changed!";
+				echo "<script>alert('$msg');</script>";
+			}
+			else {
+				$msg = "Similar password detected! Please retry with a new password!";
+				echo "<script>alert('$msg');</script>";
+			}
 		}
 		else {
 			$msg="Your current password is wrong!\nPlease try again.";
+			echo "<script>alert('$msg');</script>";
 		}
 	}
-?> -->
+?>
 
 	<!DOCTYPE html>
 	<!--start of html-->
 	<html>
 
 	<!--start of head-->
-
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -41,6 +49,16 @@ else {
 		<?php
 	  include('includes/user-head-styles.php');
 	  ?>
+		<script type="text/javascript">
+			function checkpass() {
+				if(document.changePassword.newPassword.value!=document.register.confirmPassword.value) {
+					alert('New Password and Confirm Password fields do not match');
+					document.changePassword.confirmPassword.focus();
+					return false;
+				}
+				return true;
+			}
+		</script>
 	</head>
 	<!--end of head-->
 
@@ -80,31 +98,29 @@ else {
 			</div>
 
 			<div class="row">
-				<div class="col-lg-12">
-
+				<div class="col-md-8">
 					<div class="panel panel-default">
 						<div class="panel-body">
-	            <p style="font-size:16px; color:red" align="center">
-	              <?php if($msg){ echo $msg;} ?>
-	            </p>
 	            <div class="col-md-2"></div>
 							<div class="col-md-8">
 							 <?php
 	              $ret = mysqli_query($con,"SELECT * FROM business WHERE businessID='$businessID'");
 	              while ($row = mysqli_fetch_array($ret)) {
+									$businessRegistrationDate = $row['businessRegistrationDate'];
+									$businessLastPasswordChangeDate = $row['businessLastPasswordChangeDate'];
 	              ?>
 								<form role="form" method="post" action="" name="changePassword" onsubmit="return checkpass();">
 									<div class="form-group">
 										<label><span class="fa fa-key"></span>&nbsp Current Password</label>
-										<input type="password" name="currentPassword" class=" form-control" required= "true" value="">
+										<input type="password" name="currentPassword" id="currentPassword" class=" form-control" required= "true" value="">
 									</div>
 									<div class="form-group">
 										<label><span class="fa fa-lock"></span>&nbsp New Password</label>
-										<input type="password" name="newPassword" class="form-control" value="" required="true">
+										<input type="password" name="newPassword" id="newPassword" class="form-control" value="" required="true">
 									</div>
 									<div class="form-group">
 										<label><span class="fa fa-check"></span>&nbsp Confirm Password</label>
-										<input type="password" name="confirmPassword" class="form-control" value="" required="true">
+										<input type="password" name="confirmPassword" id="confirmPassword" class="form-control" value="" required="true">
 									</div>
 	                <br>
 	                <div align="center" class="form-group has-success">
@@ -114,6 +130,20 @@ else {
 								<?php } ?>
 							</div>
 	            <div class="col-md-2"></div>
+						</div><!-- /.panel-body-->
+					</div><!-- /.panel-->
+				</div><!-- /.col-->
+				<div class="col-md-4">
+					<div class="panel panel-default">
+						<div class="panel-body easypiechart-panel">
+							<br><h4><b><span class="fa fa-pencil"></span>&nbsp Last Password Change Date:</b><br><br><?php echo $businessLastPasswordChangeDate; ?></h4><br>
+						</div><!-- /.panel-body-->
+					</div><!-- /.panel-->
+				</div><!-- /.col-->
+				<div class="col-md-4">
+					<div class="panel panel-default">
+						<div class="panel-body easypiechart-panel">
+							<br><h4><b><span class="fa fa-file-o"></span>&nbsp Registration Date:</b><br><br><?php echo $businessRegistrationDate; ?></h4><br>
 						</div><!-- /.panel-body-->
 					</div><!-- /.panel-->
 				</div><!-- /.col-->
@@ -128,13 +158,6 @@ else {
 		<!--javascript source-->
 		<script src="js/jquery-1.11.1.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
-		<script src="js/chart.min.js"></script>
-		<script src="js/chart-data.js"></script>
-		<script src="js/easypiechart.js"></script>
-		<script src="js/easypiechart-data.js"></script>
-		<script src="js/bootstrap-datepicker.js"></script>
-		<script src="js/custom.js"></script>
-		<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 
 	</body>
 	<!--end of body-->
