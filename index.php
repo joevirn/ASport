f0f47c1323442d84edb2099abed728bfce0e88d5
joevirn <69099488+joevirn@users.noreply.github.com>
2022-11-06 04:46:24 +0800

    Add index page as welcome screen for ASport.

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ASport | Home</title>
+	<link rel="icon" type="image/png" href="images/ASport.png" sizes="113x113" >
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="css/styles.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+</head>
+
+<body>
+
+	<div class="row">
+		<div class="col-md-2"></div>
+		<div class="col-md-8">
+			<a href="index.php">
+				<div class="panel panel-default">
+					<div class="panel-body easypiechart-panel">
+						<br><p><img src="images/ASport.png" width="200" height="200"></p>
+						<h1><b>ASport</b></h1>
+						<h1><tt>One-Stop Centre For Sports Facilities</tt></h1><br>
+					</div><!-- /.panel-body-->
+				</div><!-- /.panel-->
+			</a>
+		</div><!-- /.col-->
+		<div class="col-md-2"></div>
+	</div>
+	<br>
+
+	<div class="row">
+		<div class="col-md-2"></div>
+		<div class="col-md-4">
+			<a href="user-login.php" target="_blank">
+				<div class="panel panel-default">
+					<div class="panel-body easypiechart-panel">
+						<h3><b>VISIT USER PORTAL</b></h4><br>
+						<p><img src="images/userAvatar.png" width="200" height="200"></p><br>
+					</div><!-- /.panel-body-->
+				</div><!-- /.panel-->
+			</a>
+		</div><!-- /.col-->
+		<div class="col-md-4">
+			<a href="business-login.php" target="_blank">
+				<div class="panel panel-default">
+					<div class="panel-body easypiechart-panel">
+						<h3><b>VISIT BUSINESS PORTAL</b></h4><br>
+						<p><img src="images/businessAvatar.png" width="200" height="200"></p><br>
+					</div><!-- /.panel-body-->
+				</div><!-- /.panel-->
+			</a>
+		</div><!-- /.col-->
+		<div class="col-md-2"></div>
+	</div>
+	<br>
+
+		<!--include the footer-->
+	<?php include_once('includes/footer.php'); ?>
+
+
+</body>
+<!--end of body-->
+
+</html>
+<!--end of html-->
