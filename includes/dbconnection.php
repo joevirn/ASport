e1bb6bc9fbd619cacd9ca69934625335383a5e1e
joevirn <69099488+joevirn@users.noreply.github.com>
2022-11-28 05:33:45 +0800

    Add default timezone as Asia/Kuala_Lumpur for MySQL

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -3,4 +3,5 @@ $con=mysqli_connect("localhost", "root", "", "ASport");
 if(mysqli_connect_errno()){
   echo "Connection Fail".mysqli_connect_error();
 }
+date_default_timezone_set('Asia/Kuala_Lumpur');
 ?>
